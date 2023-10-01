ec315a36e594a5be4df890d98165fccdd66a7a74
mr360 <qd0097@gmail.com>
2023-10-02 06:09:09 +1100

    chore: run inital flake conf

--- flake.nix
+++ flake.nix
@@ -1,5 +1,20 @@
 {
     description = "NixOS Flake";
+    
+    nixConfig = {
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://cache.nixos.org/"
+      ];
+
+      extra-substituters = [
+        # Nix community's cache server
+        "https://nix-community.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
 
     inputs = {
     	nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
@@ -16,4 +31,4 @@
 	    };
 	};
      };
-};
+}
