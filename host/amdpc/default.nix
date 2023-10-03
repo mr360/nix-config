ac85a010574cbd4143e833aa799fb68878bd37ae
mr360 <qd0097@gmail.com>
2023-10-04 01:43:44 +1100

    refactor: move contents to defaults

---
+++ host/amdpc/default.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      ../../boot/uefi.nix
+      ../../modules/common.nix
+      ../../modules/package.nix
+      ../../modules/user.nix
+      ../../modules/virt.nix	
+    ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  networking.hostName = "amd-desktop"; 
+  networking.networkmanager.enable = true;  
+ 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.desktopManager.lxqt.enable = true;
+  services.xserver.excludePackages = [ 
+    pkgs.xterm
+  ];
+  environment.lxqt.excludePackages = [
+    pkgs.lxqt.lximage-qt
+    pkgs.lxqt.screengrab
+  ];
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e,caps:escape";
+
+  system.stateVersion = "23.05";
+}
+
--- modules/common.nix
+++ modules/common.nix
@@ -1,4 +1,11 @@
-({...}: {
+({lib, ...}: {
   nixpkgs.config.allowUnfree = true;
   hardware.opengl.enable = true;
+
+  time.timeZone = "Australia/Sydney";
+
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 })
