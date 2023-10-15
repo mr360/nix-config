68b3ae47efce9f6dc2547089c6cef73afd745c9c
mr360 <qd0097@gmail.com>
2023-10-15 15:49:43 +1100

    refactor: remove borken theme

--- module/pkgs/default.nix
+++ module/pkgs/default.nix
@@ -4,7 +4,6 @@ in {
   nixpkgs.overlays = [(final: prev: {
     localpkgs = {
       themes = {
-        windows-classic = callPackage ./themes/windows-classic-theme.nix {};
         goldy-plasma = callPackage ./themes/gtk-theme.nix {};
         chicago95 = callPackage ./themes/chicago95-theme.nix {};
         raleigh-reloaded = callPackage ./themes/raleigh-reloaded-theme.nix {};
