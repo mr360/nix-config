e955483c168a8ce27c133192321ca7190e861cdc
mr360 <qd0097@gmail.com>
2023-10-18 07:45:31 +1100

    chore: remove test themes

--- module/pkgs/default.nix
+++ module/pkgs/default.nix
@@ -4,7 +4,6 @@ in {
   nixpkgs.overlays = [(final: prev: {
     localpkgs = {
       themes = {
-        goldy-plasma = callPackage ./themes/gtk-theme.nix {};
         chicago95 = callPackage ./themes/chicago95-theme.nix {};
         raleigh-reloaded = callPackage ./themes/raleigh-reloaded-theme.nix {};
       };
