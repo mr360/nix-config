10fbdfdacfd8cdd42ef40c43e9c3d38be96756c9
mr360 <qd0097@gmail.com>
2023-12-10 11:18:02 +0000

    chore: update startvm alias window size to 1680x1050

--- home-manager/amdpc/default.nix
+++ home-manager/amdpc/default.nix
@@ -107,7 +107,7 @@
       sudo chown ${config.home.username}:users /dev/input/by-id/usb-Corsair_Corsair_K70R_Gaming_Keyboard-if02-event-kbd &&
       sudo chown ${config.home.username}:users /dev/input/by-id/usb-Logitech_USB_Receiver-if01-event-mouse &&
       virsh -c qemu:///session start win10-1080ti ; 
-      looking-glass-client win:size=1366x784'';
+      looking-glass-client win:size=1680x1050'';
   };
 
 }
