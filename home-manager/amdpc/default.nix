7fd10745ba8cd0f7a95d28622ab29bd2b319e4b5
mr360 <qd0097@gmail.com>
2024-06-23 22:35:46 +1000

    chore: remove vscode

--- home-manager/amdpc/default.nix
+++ home-manager/amdpc/default.nix
@@ -11,7 +11,6 @@
   ];
 
   home.packages = with pkgs; [
-    vscode
   ];
 
   # TODO: Issue: Needs to be run twice -- first time to create the panel.conf & another to symlink
@@ -25,9 +24,8 @@
         apps\1\desktop=${pkgs.pcmanfm-qt}/share/applications/pcmanfm-qt.desktop
         apps\2\desktop=${pkgs.google-chrome}/share/applications/google-chrome.desktop
         apps\3\desktop=${pkgs.lxqt.qterminal}/share/applications/qterminal.desktop
-        apps\4\desktop=${pkgs.vscode}/share/applications/code.desktop
-        apps\5\desktop=${pkgs.libsForQt5.okular}/share/applications/org.kde.okular.desktop
-        apps\size=5
+        apps\4\desktop=${pkgs.libsForQt5.okular}/share/applications/org.kde.okular.desktop
+        apps\size=4
         type=quicklaunch
       '';
     };
